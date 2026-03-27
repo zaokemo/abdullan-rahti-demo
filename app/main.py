5aeeb26b2b9422afdb9f8f4975cde7c6d8653f46
zaokemo <Thelionkemo@gmail.com>
2026-03-27 12:02:37 +0200

    code chanllage 2.2

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -13,6 +14,10 @@ def hello():
 @app.get("/api/ip")
 def ip(request: Request):
     return { "ip": request.client.host }
+
+@app.get("/ip", response_class=HTMLResponse)
+def ip(request: Request):
+    return f"<h1>Din ip är {request.client.host}</h1>"
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
