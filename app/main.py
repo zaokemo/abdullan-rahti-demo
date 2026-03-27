348e5e0cd120cb47267db9e66e1b5d25cd80abe9
zaokemo <Thelionkemo@gmail.com>
2026-03-27 11:58:37 +0200

    code challange 2

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Moro!", "v": "0.1" }
+    return { "msg": "Moro local docker!" }
 
+@app.get("/hello")
+def hello():
+    return { "msg": "what`s up" }
+
+@app.get("/api/ip")
+def ip(request: Request):
+    return { "ip": request.client.host }
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
